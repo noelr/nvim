cc0b43c4cc79d0637d23557131b8dbc970fc682d
Noel Rohrbach <noel@noelr.ch>
2026-07-10 09:15:41 +0200

    we dotnet now

--- init.lua
+++ init.lua
@@ -1,4 +1,7 @@
 vim.o.wrap = false
+-- The built-in sql ftplugin maps <C-C>{a,k,t,c,...} for omni-completion, which
+-- shadows <c-c> and makes it wait timeoutlen. Move that trigger off <C-C>.
+vim.g.ftplugin_sql_omni_key = "<C-J>"
 vim.keymap.set("n", "<c-c>", ":nohlsearch<cr>", { silent = true })
 vim.keymap.set("i", "<c-c>", "<esc>", { silent = true })
 vim.o.number = true
@@ -9,8 +12,10 @@ vim.o.incsearch = true
 vim.o.scrolloff = 5
 vim.o.winwidth = 120
 
+-- require('vim._core.ui2').enable()
+vim.opt.runtimepath:prepend("/Users/noel/devel/inline")
+
 vim.diagnostic.config({
-  -- virtual_lines = true,
   virtual_text = { current_line = true }
 })
 
@@ -22,16 +27,28 @@ vim.pack.add({
   { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
   { src = "https://github.com/neovim/nvim-lspconfig" },
   { src = "https://github.com/mason-org/mason.nvim" },
+  { src = "https://github.com/nvim-lua/plenary.nvim" },
+  { src = "https://github.com/folke/snacks.nvim" },
+  { src = "https://github.com/GustavEikaas/easy-dotnet.nvim" },
+  { src = "https://codeberg.org/mfussenegger/nvim-dap.git" },
 })
 
 vim.cmd([[colorscheme tokyonight-moon]])
 
+require("easy-dotnet").setup({
+  debugger = {
+    -- netcoredbg fork with better funceval; sharpdbg loses the stopped
+    -- state on REPL assignments (as of easydotnet 3.4.11)
+    engine = "dncdbg",
+  },
+})
+-- inline variable values + `T` variable viewer while stopped at a breakpoint
+require("easy-dotnet.netcoredbg").register_dap_variables_viewer()
 require("mason").setup()
 
 vim.lsp.enable('html')
 vim.lsp.enable('lua_ls')
 vim.lsp.enable('ocamllsp')
-vim.lsp.enable('omnisharp')
 vim.lsp.enable('solargraph')
 vim.lsp.config('ts_ls', {
   filetypes = { "typescript", "typescriptreact" },
