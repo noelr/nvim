e7089862f2a90de032bcaf8e0562131791b1a8e7
Noel Rohrbach <noel@noelr.ch>
2026-07-11 19:47:10 +0200

    more dotnetting

--- init.lua
+++ init.lua
@@ -11,6 +11,7 @@ vim.o.smartcase = true
 vim.o.incsearch = true
 vim.o.scrolloff = 5
 vim.o.winwidth = 120
+vim.o.completeopt = "menuone,noselect,popup,fuzzy"
 
 -- require('vim._core.ui2').enable()
 vim.opt.runtimepath:prepend("/Users/noel/devel/inline")
@@ -31,9 +32,27 @@ vim.pack.add({
   { src = "https://github.com/folke/snacks.nvim" },
   { src = "https://github.com/GustavEikaas/easy-dotnet.nvim" },
   { src = "https://codeberg.org/mfussenegger/nvim-dap.git" },
+  { src = "https://github.com/mistweaverco/kulala.nvim" },
+  { src = "https://github.com/stevearc/oil.nvim.git" },
 })
 
 vim.cmd([[colorscheme tokyonight-moon]])
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "razor", "cs", "html" },
+  callback = function() pcall(vim.treesitter.start) end,
+})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client and client:supports_method("textDocument/completion") then
+      vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+    end
+  end,
+})
+
+vim.keymap.set("i", "<C-Space>", function() vim.lsp.completion.get() end,
+  { desc = "Trigger LSP completion" })
 
 require("easy-dotnet").setup({
   debugger = {
@@ -44,6 +63,9 @@ require("easy-dotnet").setup({
 })
 -- inline variable values + `T` variable viewer while stopped at a breakpoint
 require("easy-dotnet.netcoredbg").register_dap_variables_viewer()
+require("kulala").setup()
+require("oil").setup()
+
 require("mason").setup()
 
 vim.lsp.enable('html')
@@ -60,6 +82,8 @@ vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Live
 vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Buffers" })
 vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { desc = "Help tags" })
 vim.keymap.set("n", "<leader>fs", "<cmd>Telescope git_status<cr>", { desc = "Git status" })
+vim.keymap.set("n", "<leader>fk", "<cmd>Telescope keymaps<cr>", { desc = "Keymaps" })
 vim.keymap.set("n", "<leader>fn", function()
   require('telescope.builtin').find_files({ cwd = vim.fn.stdpath('config') })
 end, { desc = "Neovim config" })
+vim.keymap.set("n", "<leader>hr", function() require("kulala").run() end, { desc = "Run http request" })
